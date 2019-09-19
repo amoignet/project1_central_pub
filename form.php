6baeb77cc3a2fde53e181790b6a30844880f5f3e
Céline Godichon <celine.godichon@gmail.com>
2019-09-19 18:14:55 +0200

    THE LAST ONE

--- form.php
+++ form.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="style.css">
   <link rel="stylesheet" href="responsive.css">
+  <link rel="stylesheet" href="form.css">
   <script type="text/javascript">
     function myFunction() { 
   
@@ -24,13 +25,13 @@
 <body>
     <nav id="nav">
         <div class="smallnav">
-          <div class="logonav"><a href="index.html#nav"><img alt="logo" src="logo.png"></a></div>
+          <div class="logonav"><a href="index.php#nav"><img alt="logo" src="logo.png"></a></div>
           <div class="icon"><a href="javascript:void(0);" onclick="myFunction()"><i class="fa fa-bars"></i></a></div>
         </div>
           <div id="menuderoulant">
               <ul class="fullnavbutton">
-                  <li><a href="#presentation" class="navbutton">Presentation</a></li>
-                  <li><a href="#reservation" class="navbutton">Reservation</a></li>
+                  <li><a href="index.php#presentation" class="navbutton">Presentation</a></li>
+                  <li><a href="index.php#reservation" class="navbutton">Reservation</a></li>
                   <li><a href="menu.html" class="navbutton">Notre carte</a></li>
                   <li><a href="recrutement.html" class="navbutton">Jobs</a></li>
               </ul>
@@ -45,28 +46,28 @@
       </nav>
       
 
-  <section class="text_bannière">
-    <img class="bannière" src="https://zupimages.net/up/19/38/ilzq.jpg" alt="Central Pub vue extérieur">
-    <h1>WELCOME</h1>
+  <section class="bannière_resa">
+    <img class="bannière" src="https://zupimages.net/up/19/38/6set.jpeg" alt="Some beer on a table">
+    <h1 class="resa_confirmed"> Votre réservation a bien été prise en compte</h1>
   </section>
     
-<section>
-<h2> Votre reservation a bien été prise en compte</h2>
+<section class="bonjour">
+  <div class ="text_form">
+<?php $newDate = date("d-m-Y", strtotime($_GET['date']));?>
 
-<div>
-    Bonjour <?php echo $_GET['prenom'];?>, votre reservation a bien été transmise. Nous vous attendons avec grand plaisir le <?php echo $_GET['date'];?>
-    Une table pour <?php echo $_GET['NombreDePersonnes'];?> sera prête pour vous à <?php echo $_GET['horaire'];?>.
-    A bientot !
+    Bonjour <strong> <?php echo $_GET['prenom'];?> </strong>, <br> votre réservation a bien été transmise. <br> Nous vous attendons avec grand plaisir le <strong> <?php echo $newDate;?> </strong>. <br>
+    Une table pour <strong> <?php echo $_GET['NombreDePersonnes'];?></strong> personnes sera prête pour vous à <strong> <?php echo $_GET['horaire'];?>.</strong> <br>
+    A bientôt !
 </div>
 
 </section>
   <footer>
-    <a href="#nav"><img alt="logo" src="logo.png" class="logofooter"></a>
+    <a href="index.php#nav"><img alt="logo" src="logo.png" class="logofooter"></a>
     <div id="cartevisite">
 
-      <p>7 Quai de Queyries 33100 BORDEAUX</br>
-        <a href="tel:+33557803800">05.57.80.38.00</a></br>
-        Service continu de 11h30 - 0h30</br> Ouvert tous les jours de 9h - 2h </p>
+      <p>7 Quai de Queyries 33100 BORDEAUX<br>
+        <a href="tel:+33557803800">05.57.80.38.00</a><br>
+        Service continu de 11h30 - 0h30<br> Ouvert tous les jours de 9h - 2h </p>
     </div>
     <div class="sousfooter">
       <div class="reseaux reseauxFooter">
